a134633c4a50d71bec2dc0565c8930c4d1b7a763
gideonvictor <q@gideonvictor.com>
2021-11-19 18:26:43 -0800

    fixed /body index

--- assets/js/ddd3.js
+++ assets/js/ddd3.js
@@ -3,6 +3,7 @@
 var margin = {top: -40, right: 30, bottom: 60, left: 0},
     width = 780 - margin.left - margin.right,
     height = 448 - margin.top - margin.bottom;
+
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
   .append("svg")
@@ -19,8 +20,8 @@ d3.csv("https://gideonvictor.com/kings_1.csv", function(data) {
   var x = d3.scaleLinear()
     .domain([-100, 0])
     .range([ 10, width/2 ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+  // svg.append("g")
+  //   .attr("transform", "translate(0," + height + ")")
     // .call(d3.axisBottom(x));
 
   // Add Y axis
