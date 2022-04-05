307293bbd1210b48fc443711d7175de2112ceedf
gideonvictor <q@gideonvictor.com>
2022-04-04 22:34:00 -0700

    Flames Data

--- sandboxassets/js/ddd3.js
+++ sandboxassets/js/ddd3.js
@@ -103,7 +103,7 @@ d3.csv("https://gideonvictor.com/Opponent.csv", function(data) {
   // Prepare a color palette
   var color = d3.scaleLinear()
       .domain([minnn, maxxx]) // Number of points in the bin?
-      .range(["Transparent",  "#004C97"])
+      .range(["Transparent",  "#c8102E"])
 
 
 
